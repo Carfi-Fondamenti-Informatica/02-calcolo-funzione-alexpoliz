013a2cb265cee7948bababce0347ce87c358988c
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-11-14 15:34:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-   float a=0, b=0; x=0, y=0, z=0;
+   float a=0, b=0, x=0, y=0, z=0;
    cin>>a>>b>>x>>y;
    if((x<0)&&(y>0)){
       z=(a*x)-(b*y);
