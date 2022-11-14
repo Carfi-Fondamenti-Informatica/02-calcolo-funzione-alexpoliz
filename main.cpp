7a58c0fcefae8871531eedeb268f0bcd617799b7
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-11-14 15:32:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   float a=0, b=0, x=0; y=0, z=0;
+   float a=0, b=0; x=0, y=0, z=0;
    cin>>a>>b>>x>>y;
-   if((x<0)&&(y>o)) {
+   if((x<0)&&(y>0)){
       z=(a*x)-(b*y);
       cout<<z<<endl;
-   }
-   else if((x>=0)&&(y<=0)){
+   }else if ((x>=0)&&(y<=0)){
       z=(a*x*x)-(b*y*y);
       cout<<z<<endl;
-   } else{
-      z=(a*x)+(b*y*y);
-      cout<<z<<endl;
+   }else{
+      z= (a*x)+(b*y*y);
+      cout<< z<< endl;
    }
+  
    return 0;
 }
